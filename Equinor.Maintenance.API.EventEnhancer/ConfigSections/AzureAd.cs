0f91220a1b8a563489d681c2c6152d5b5c27155f
Henri Siik <hsii@equinor.com>
2022-08-25 14:32:24 +0200

    feat: added authentication and authorization
    using v2 tokens
    
    also added user logging, but will probably need to be modified in order to capture system user. It is mostly a placeholder since only our own system will call the event enhancer, but can be repurposed for other logging.

--- Equinor.Maintenance.API.EventEnhancer/ConfigSections/AzureAd.cs
+++ Equinor.Maintenance.API.EventEnhancer/ConfigSections/AzureAd.cs
@@ -2,9 +2,11 @@ using JetBrains.Annotations;
 
 namespace Equinor.Maintenance.API.EventEnhancer.ConfigSections;
 
-public class AzureConfig
+public class AzureAd
 {
     public string   ClientId              { get; [UsedImplicitly] set; } = "";
+    public string   TenantId              { get; [UsedImplicitly] set; } = "";
+    public string   Instance              { get; [UsedImplicitly] set; } = "";
     public string   ClientSecret          { get; [UsedImplicitly] set; } = "";
     public string[] AllowedWebHookOrigins { get; [UsedImplicitly] set; } = { };
 }
--- Equinor.Maintenance.API.EventEnhancer/Program.cs
+++ Equinor.Maintenance.API.EventEnhancer/Program.cs
@@ -1,33 +1,50 @@
 using System.Net.Mime;
 using Equinor.Maintenance.API.EventEnhancer.ConfigSections;
+using Equinor.Maintenance.API.EventEnhancer.Logging;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
+using Microsoft.Identity.Web;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Host.UseSerilog((ctx, lc) => lc
-                                     .WriteTo.Console());
+builder.Services.AddTransient<HttpContextEnricher>();
+builder.Services.AddHttpContextAccessor();
+builder.Host.UseSerilog((_, services, lc) =>
+                        {
+                            lc
+                                .Enrich.FromLogContext()
+                                .Enrich.With(services.GetService<HttpContextEnricher>())
+                              .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} by user {User}{NewLine}{Exception}");
+                        });
 
-builder.Services.AddOptions<AzureConfig>()
-       .Bind(builder.Configuration.GetSection(nameof(AzureConfig)))
+builder.Services.AddOptions<AzureAd>()
+       .Bind(builder.Configuration.GetSection(nameof(AzureAd)))
        .Validate(config => config.AllowedWebHookOrigins.Any(), "AzureConfig:AllowedWebHookOrigins must be populated")
        .Validate(config => !string.IsNullOrWhiteSpace(config.ClientId), "AzureConfig:ClientId must be populated")
        .Validate(config => !string.IsNullOrWhiteSpace(config.ClientSecret), "AzureConfig:ClientSecret must be populated")
        .ValidateOnStart();
+
+
+builder.Services
+       .AddMicrosoftIdentityWebApiAuthentication(builder.Configuration, 
+                                                 subscribeToJwtBearerMiddlewareDiagnosticsEvents:builder.Environment.IsDevelopment());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGet("/", () => new OkObjectResult("Hello from Mini Api"))
 .WithName("HelloApi");
 
 app.MapMethods("/", new []{"OPTIONS"},
-               (IOptions<AzureConfig> azureConfig, HttpContext ctx) =>
+               (IOptions<AzureAd> azureConfig, HttpContext ctx) =>
                {
                    ctx.Response.StatusCode = StatusCodes.Status403Forbidden; //forbidden makes more sense than 405
 
@@ -42,6 +59,7 @@ app.MapMethods("/", new []{"OPTIONS"},
 
                    return Task.CompletedTask;
                })
-   .WithName("WebHookOptions");
+   .WithName("WebHookOptions")
+   .RequireAuthorization();
 
 app.Run();
