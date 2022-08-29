8f403a30c624d759427e9cd26d4e1f0b47c5a103
Henri Siik <2461675+hekka@users.noreply.github.com>
2022-08-29 14:05:33 +0200

    fix|feat: logging of origin header and application insight added (#3)
    * fix: added logging of origin header
    
    changed route of endpoints, fixed some magic strings
    
    * feat: added ApplicationInsight logging

---
+++ Equinor.Maintenance.API.EventEnhancer/Logging/LogOriginHeader.cs
@@ -0,0 +1,20 @@
+using Equinor.Maintenance.API.EventEnhancer.Constants;
+
+namespace Equinor.Maintenance.API.EventEnhancer.Logging;
+
+public class LogOriginHeader : IMiddleware
+{
+    private readonly ILogger<LogOriginHeader> _logger;
+
+    public LogOriginHeader(ILogger<LogOriginHeader> logger) { _logger = logger; }
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        await next.Invoke(context);
+        
+        if (context.Request.Headers.TryGetValue(Names.WebHookRequestHeader, out var webHookOrigin)) 
+            _logger.LogTrace("Header {HeaderName}: {WebHookOrigin}", Names.WebHookRequestHeader,webHookOrigin);
+        else
+            _logger.LogWarning("Could not find header: {HeaderName}", Names.WebHookRequestHeader);
+        
+    }
+}
--- Equinor.Maintenance.API.EventEnhancer/Program.cs
+++ Equinor.Maintenance.API.EventEnhancer/Program.cs
@@ -1,23 +1,28 @@
 using System.Net.Mime;
 using Equinor.Maintenance.API.EventEnhancer.ConfigSections;
+using Equinor.Maintenance.API.EventEnhancer.Constants;
 using Equinor.Maintenance.API.EventEnhancer.Logging;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Identity.Web;
 using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddTransient<HttpContextEnricher>();
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration);
 builder.Host.UseSerilog((_, services, lc) =>
                         {
                             lc
                                 .Enrich.FromLogContext()
-                                .Enrich.With(services.GetService<HttpContextEnricher>())
-                              .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} by user {User}{NewLine}{Exception}");
+                                .Enrich.With(services.GetRequiredService<HttpContextEnricher>())
+                                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} by user {User}{NewLine}{Exception}")
+                                .WriteTo.ApplicationInsights(services.GetRequiredService<TelemetryConfiguration>(), TelemetryConverter.Traces, restrictedToMinimumLevel:LogEventLevel.Warning);
                         });
 
 builder.Services.AddOptions<AzureAd>()
@@ -26,36 +31,40 @@ builder.Services.AddOptions<AzureAd>()
        .Validate(config => !string.IsNullOrWhiteSpace(config.ClientId), "AzureConfig:ClientId must be populated")
        .Validate(config => !string.IsNullOrWhiteSpace(config.ClientSecret), "AzureConfig:ClientSecret must be populated")
        .ValidateOnStart();
-
+builder.Services.AddScoped<LogOriginHeader>();
 
 builder.Services
-       .AddMicrosoftIdentityWebApiAuthentication(builder.Configuration, 
-                                                 subscribeToJwtBearerMiddlewareDiagnosticsEvents:builder.Environment.IsDevelopment());
+       .AddMicrosoftIdentityWebApiAuthentication(builder.Configuration,
+                                                 subscribeToJwtBearerMiddlewareDiagnosticsEvents: builder.Environment.IsDevelopment());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<LogOriginHeader>();
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/", () => new OkObjectResult("Hello from Mini Api"))
-.WithName("HelloApi");
+//var pattern = $"{(app.Environment.IsDevelopment() ? string.Empty : "/maintenance-api-event-driven-internal")}/maintenance-events";
+const string pattern = "/maintenance-events";
 
-app.MapMethods("/", new []{"OPTIONS"},
+app.MapPost(pattern, () => new OkObjectResult("Hello from Mini Api"))
+   .WithName("HelloApi");
+
+app.MapMethods(pattern,
+               new[] { HttpMethod.Options.ToString() },
                (IOptions<AzureAd> azureConfig, HttpContext ctx) =>
                {
-                   ctx.Response.StatusCode = StatusCodes.Status403Forbidden; //forbidden makes more sense than 405
+                   ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
 
-                   if (!ctx.Request.Headers.TryGetValue("WebHook-Request-Origin", out var webHookOrigin)
+                   if (!ctx.Request.Headers.TryGetValue(Names.WebHookRequestHeader, out var webHookOrigin)
                        || !azureConfig.Value.AllowedWebHookOrigins.Contains(webHookOrigin.ToString())) return Task.CompletedTask;
-                   
-                   
-                   ctx.Response.Headers.Allow       = new StringValues("GET");
+
+                   ctx.Response.Headers.Allow       = new StringValues(HttpMethod.Post.ToString());
                    ctx.Response.Headers.ContentType = new StringValues(MediaTypeNames.Application.Json);
                    ctx.Response.StatusCode          = StatusCodes.Status200OK;
-                   ctx.Response.Headers.TryAdd("WebHook-Allowed-Origin", webHookOrigin);
+                   ctx.Response.Headers.TryAdd(Names.WebHookAllowHeader, webHookOrigin);
 
                    return Task.CompletedTask;
                })
