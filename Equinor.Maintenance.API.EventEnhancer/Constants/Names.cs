0a5d46c51c62c72f02b2803edfd5e39f7e767c15
Henri Siik <hsii@equinor.com>
2022-10-06 12:16:01 +0200

    fix: header name

--- Equinor.Maintenance.API.EventEnhancer/Constants/Names.cs
+++ Equinor.Maintenance.API.EventEnhancer/Constants/Names.cs
@@ -3,7 +3,7 @@ namespace Equinor.Maintenance.API.EventEnhancer.Constants;
 public class Names
 {
     public const string WebHookRequestHeader = "Webhook-Request-Origin";
-    public const string WebHookAllowHeader = "Webhook-Allow-Origin";
+    public const string WebHookAllowHeader = "Webhook-Allowed-Origin";
     public const string Topic = "maintenance-events";
     public const string MainteanceApi = "MaintenanceApiServiceName";
 }
