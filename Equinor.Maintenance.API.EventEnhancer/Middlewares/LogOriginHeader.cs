d245abb2c8855636d7c1e514c22f67c0dfca8dd6
David Gordon Ramsay <2072677+davidramsay95@users.noreply.github.com>
2023-02-15 08:17:10 +0100

    Feat: Add properties from user story (#16)

--- Equinor.Maintenance.API.EventEnhancer/Middlewares/LogOriginHeader.cs
+++ Equinor.Maintenance.API.EventEnhancer/Middlewares/LogOriginHeader.cs
@@ -19,6 +19,10 @@ public class LogOriginHeader : IMiddleware
         {
             await next.Invoke(context);
         }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+        }
         finally
         {
             request.Body.Position = 0;
